f87f7e45c3b45332e649921306116ae3761b7430
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-29 12:53:06 +0900

    Sisimai::RFC3464 now can decode a bounce mail generated by Bigfoot #557

--- t/900-modules.pl
+++ t/900-modules.pl
@@ -14,7 +14,6 @@ sub list {
             Lhost/AmazonWorkMail.pm
             Lhost/Aol.pm
             Lhost/ApacheJames.pm
-            Lhost/Bigfoot.pm
             Lhost/Biglobe.pm
             Lhost/Courier.pm
             Lhost/Domino.pm

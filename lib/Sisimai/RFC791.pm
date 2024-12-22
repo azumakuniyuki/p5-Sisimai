bc98b794ac719b3fd5168ccbee9633fff5a9e954
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-22 10:42:20 +0900

    Sisimai::String->ipv4 has been moved to Sisimai::RFC791->find #560

--- lib/Sisimai/RFC791.pm
+++ lib/Sisimai/RFC791.pm
@@ -3,6 +3,49 @@ use v5.26;
 use strict;
 use warnings;
 
+sub find {
+    # Find an IPv4 address from the given string
+    # @param    [String] argv1  String including an IPv4 address
+    # @return   [Array]         List of IPv4 addresses
+    # @since v5.0.0
+    my $class = shift;
+    my $argv0 = shift || return undef; return [] if length $argv0 < 7;
+    my $ipv4a = [];
+
+    for my $e ( "(", ")", "[", "]", "," ) {
+        # Rewrite: "mx.example.jp[192.0.2.1]" => "mx.example.jp 192.0.2.1"
+        my $p0 = index($argv0, $e); next if $p0 < 0;
+        substr($argv0, $p0, 1, " ");
+    }
+
+    IP4A: for my $e ( split(" ", $argv0) ) {
+        # Find string including an IPv4 address
+        next if index($e, ".") == -1;   # IPv4 address must include "." character
+
+        my $lx = length $e; next if $lx < 7 || $lx > 17; # 0.0.0.0 = 7, [255.255.255.255] = 17
+        my $cu = 0;     # Cursor for seeking each octet of an IPv4 address
+        my $as = "";    # ASCII Code of each character
+        my $eo = "";    # Buffer of each octet of IPv4 Address
+
+        while( $cu < $lx ) {
+            # Check whether each character is a number or "." or not
+            $as = ord substr($e, $cu++, 1);
+            if( $as < 48 || $as > 57 ) {
+                # The character is not a number(0-9)
+                next IP4A if     $as != 46; # The character is not "."
+                next      if     $eo eq ""; # The current buffer is empty
+                next IP4A if int $eo > 255; # The current buffer is greater than 255
+                $eo = "";
+                next;
+            }
+            $eo .= chr $as;
+            next IP4A if int $eo > 255;
+        }
+        push @$ipv4a, $e;
+    }
+    return $ipv4a;
+}
+
 1;
 __END__
 =encoding utf-8
@@ -15,13 +58,20 @@ Sisimai::RFC791 - IP related class
 
     use Sisimai::RFC791;
 
-    print Sisimai::RFC791->find("mx7.example.jp:[192.0.2.27]"); # "192.0.2.27"
+    my $p = Sisimai::RFC791->find("mx7.example.jp:[192.0.2.27]"); # ["192.0.2.27"]
 
 =head1 DESCRIPTION
 
 C<Sisimai::RFC791> is a class related to IP
 
 =head1 CLASS METHODS
+
+=head2 C<B<ipv4(I<String>)>>
+
+C<ipv4> method return all the IPv4 address found in the given string.
+
+    my $v = "connection refused from 192.0.2.1, DNSBL returned 127.0.0.2";
+    my $p = Sisimai::RFC791->find($v); # ["192.0.2.1", "127.0.0.2"]
 
 =head1 AUTHOR
 
